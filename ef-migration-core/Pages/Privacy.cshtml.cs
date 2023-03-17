fd6c9154a254bb3c232c16942ec0d3256ec44fe5
Paul Faye <fayepaul234@gmail.com>
2023-03-16 20:22:52 -0400

    Add project files.

---
+++ ef-migration-core/Pages/Privacy.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ef_migration_core.Pages {
+    public class PrivacyModel : PageModel {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger) {
+            _logger = logger;
+        }
+
+        public void OnGet() {
+        }
+    }
+}
